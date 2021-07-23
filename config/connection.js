0ec6a9a607e82a1113b1e87b5fa52452ae0a7421
pablo de la cruz <pablodlc@gmail.com>
2021-07-23 16:56:35 -0700

    init heroku commit

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// adding Sequelize
+const Sequelize = require('sequelize');
+
+// adding dotenv
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3333
+    });
+}
+
+module.exports = sequelize;
